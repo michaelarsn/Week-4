138e6431893fdcafa2efabf0918448bac1c7294e
michaelarsn <111519843+michaelarsn@users.noreply.github.com>
2022-09-29 09:11:07 +0200

    Add files via upload

---
+++ homework4.js
@@ -0,0 +1,13 @@
+alert('What is the weather like today?');
+
+let WeatherToday = prompt('Is it raining or not?');
+if (WeatherToday == 'yes') {
+    alert ('Have a cup of tea and read a book.');
+}
+else if (WeatherToday == 'no') {
+    alert ('Enjoy the sunshine while it lasts.');
+} else {
+    alert ('Sorry, we could not read that. Try again!');
+}
+
+alert ('Take care!');
